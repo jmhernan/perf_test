d45ed479cb12ea0c084b6ca161c103d653c3bf51
Jose Hernandez <jmhernandeze@gmail.com>
2021-10-22 11:56:50 -0700

    auto correlation function with k

--- autocorrelation.py
+++ autocorrelation.py
@@ -1,18 +1,15 @@
-import statistics as st 
 import numpy as np 
-
-test_data = np.loadtxt('lew.txt')
+from scipy.ndimage.interpolation import shift
+import statistics as st
+import math
 
 def sample_mean(data_array):
     mean = data_array.sum()/data_array.size
     return mean 
 
-data_array = np.array([1,2,3,4,5])
-np.sum([(a-sample_mean(data_array))**2 for a in data_array]) # try this approach 
 
 def auto_cor(data_array, k):
-    (np.sum((test_data - sample_mean(test_data))) * np.sum(np.delete(test_data,0)-sample_mean(test_data)))/np.sum([(a-sample_mean(test_data))**2 for a in test_data])
-
-
-first_order_res = np.sum(test_data - sample_mean(test_data))
-    
+    auto_correlation = np.sum((data_array - sample_mean(data_array)) * 
+        (shift(data_array, k, cval=0)-sample_mean(data_array)))/ \
+        np.sum([(a-sample_mean(data_array))**2 for a in data_array])
+    return auto_correlation
